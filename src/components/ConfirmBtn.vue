d57701329cd48c3006efba68b13a694076b9b598
airmanxtw <airmanx@stust.edu.tw>
2022-10-20 22:31:41 +0800

    init

---
+++ src/components/ConfirmBtn.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-btn v-bind="$attrs" @click="model=true">
+        <v-icon v-if="props.icon!=null">mdi-{{props.icon}}</v-icon>
+        {{props.label}}
+    </v-btn>
+    <v-dialog v-model="model" max-width="500">
+        <v-card>
+            <v-card-text>
+                <slot name="message"></slot>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary" @click="()=>{model=false;emits('confirm')}">confirm</v-btn>
+                <v-btn color="secondary" @click="model=false">cancel</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+<script setup>
+import { ref } from 'vue';
+let model = ref(false);
+const emits = defineEmits(['confirm']);
+const props = defineProps({
+    icon: {
+        type: String,
+        default: null
+    },
+    label: {
+        type: String,
+        default: null
+    }
+})
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img :src="logo" class="my-3" contain height="200" />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to the Vuetify 3 Beta
+        </h1>
+
+        <h4>Vite Preview</h4>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col>
+        <confirm-btn color="primary" @confirm="confirm" icon="delete" label="delete">
+          <template v-slot:message>
+            <p class="text-h5">
+              Are you sure you want to delete this item?
+            </p>
+          </template>
+        </confirm-btn>
+
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+import logo from '../assets/logo.svg';
+import confirmBtn from './ConfirmBtn.vue';
+const confirm = () => {
+  alert('confirm click!');
+}
+</script>
+
+
+
+
+
