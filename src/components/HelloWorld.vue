640f3f4a49d4a1d40935b0612be82c3b07768d52
airmanxtw <airmanx@stust.edu.tw>
2022-10-20 23:28:25 +0800

    add readme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,17 +9,9 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to the Vuetify 3 Beta
         </h1>
-
-        <h4>Vite Preview</h4>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
       </v-col>
 
-      <v-col>
+      <v-col cols="12">
         <confirm-btn color="primary" @confirm="confirm" icon="delete" label="delete">
           <template v-slot:message>
             <p class="text-h5">
@@ -27,9 +19,7 @@
             </p>
           </template>
         </confirm-btn>
-
       </v-col>
-
     </v-row>
   </v-container>
 </template>
