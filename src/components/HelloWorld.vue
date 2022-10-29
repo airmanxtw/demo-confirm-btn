6bdedc6637173008b26dfce4686b141eb6e9907a
airmanxtw <airmanx@stust.edu.tw>
2022-10-29 22:17:34 +0800

    add css animate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
+        <h1 class="display-2 font-weight-bold mb-3 text-focus-in">
           Welcome to the Vuetify 3 Beta
         </h1>
       </v-col>
@@ -31,6 +31,23 @@ const confirm = () => {
   alert('confirm click!');
 }
 </script>
+<style>
+.text-focus-in {
+  animation: text-focus-in 1.5s cubic-bezier(0.550, 0.085, 0.680, 0.530) 0.2s both;
+}
+
+@keyframes text-focus-in {
+  0% {
+    filter: blur(12px);
+    opacity: 0;
+  }
+
+  100% {
+    filter: blur(0px);
+    opacity: 1;
+  }
+}
+</style>
 
 
 
