b32e03609ad96e7359de0428c47783b6a0fea34c
airmanxtw <airmanx@stust.edu.tw>
2022-12-06 15:07:06 +0800

    i18n to plugin

--- src/main.js
+++ src/main.js
@@ -1,29 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import { createI18n } from "vue-i18n";
-const i18n = createI18n({
-  legacy: false,
-  locale: "tw", // 设置地区
-  messages: {
-    en: {
-      delete: "DELETE",
-      title:"Welcome to the Vuetify 3",
-      language:"language",
-      confirm:"confirm",
-      cancel:"cancel",
-      deleteConfirm:"Are you sure you want to delete this item?"
-    },
-    tw: {
-      delete: "刪除",
-      title:"歡迎使用Vuetify3",
-      language:"語系",
-      confirm:"確定",
-      cancel:"取消",
-      deleteConfirm:"確定要刪除這個項目嗎?"
-    },
-  },
-});
+import i18n from "./plugins/i18n";
 
 import { loadFonts } from "./plugins/webfontloader";
 
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,22 +1,26 @@
-import {createI18n} from 'vue-i18n'
-const langs = {
-  en: {
-    title: {
-      delete: "delete",
-    },
-  },
-  tw: {
-    title: {
-      delete: "刪除",
-    },
-  },
-};
-
+import { createI18n } from "vue-i18n";
 const i18n = createI18n({
-    legacy:false,
-    locale: 'tw', // 设置地区
-    langs, // 设置地区信息
-})
+  legacy: false,
+  locale: "tw", // 设置地区
+  messages: {
+    en: {
+      delete: "DELETE",
+      title:"Welcome to the Vuetify 3",
+      language:"language",
+      confirm:"confirm",
+      cancel:"cancel",
+      deleteConfirm:"Are you sure you want to delete this item?"
+    },
+    tw: {
+      delete: "刪除",
+      title:"歡迎使用Vuetify3",
+      language:"語系",
+      confirm:"確定",
+      cancel:"取消",
+      deleteConfirm:"確定要刪除這個項目嗎?"
+    },
+  },
+});
 
 export default i18n;
 
